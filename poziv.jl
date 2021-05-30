41fb0160c093832a23145b8767d8ee0c04505435
ramalama99 <79934218+ramalama99@users.noreply.github.com>
2021-05-30 15:04:10 +0200

    poziv.jl

--- poziv.jl
+++ poziv.jl
@@ -1,9 +1,9 @@
 include("bucketSort.jl")
 
 A = Vector{Int}(ceil.(Int, rand(30)*100))       #pravi niz od 30 integera u range-u: [1, 100]
-#A = [1, 13, 22, 14, 99, 100]                   
+#A = [1, 13, 22, 14, 99, 100]                   #ili samostalno iskucati
+println("\nNesortirani niz A:\n\n$A")
 
-B = bucketSort!(A)                              #ova linije ne treba inace, za projekat je potrebno da se umesto nje kuca 7. linija, ovde radim ovako kako bih mogao da vidim sta mi se desava sa B, odnosno bucketima
-#bucketSort!(A)
-display(B)  
-println("Niz A nakon sortiranja bucketSort metodom: $A")
+bucketSort!(A)                              
+println("\nNiz A nakon sortiranja bucketSort metodom:\n\n$A")
+
